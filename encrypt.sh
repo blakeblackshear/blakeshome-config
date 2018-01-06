7228e25d769bf2e5117cefce7ffbe9897ff2e3e0
Blake Blackshear <blakeb@blakeshome.com>
2018-01-06 11:48:19 -0600

    add google oauth to homeassistant

--- encrypt.sh
+++ encrypt.sh
@@ -2,9 +2,10 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-read -s -p "Password:" BLAKESHOME_PASS
-
-export BLAKESHOME_PASS
+if [ -z ${BLAKESHOME_PASS+x} ]; then 
+    read -s -p "Password:" BLAKESHOME_PASS
+    export BLAKESHOME_PASS
+fi
 
 /usr/bin/openssl enc -aes-256-cbc -salt -in $DIR/home-assistant/secrets.yaml -out $DIR/home-assistant/secrets.yaml.enc -pass env:BLAKESHOME_PASS
 /usr/bin/openssl enc -aes-256-cbc -salt -in $DIR/DOCKER.env -out $DIR/DOCKER.env.enc -pass env:BLAKESHOME_PASS
