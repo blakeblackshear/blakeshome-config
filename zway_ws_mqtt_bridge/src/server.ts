b52dda8966b752a1e6cc50aa985cea073053820f
Blake Blackshear <blakeb@blakeshome.com>
2018-04-26 17:33:59 -0500

    update mqtt topics

--- zway_ws_mqtt_bridge/src/server.ts
+++ zway_ws_mqtt_bridge/src/server.ts
@@ -1,61 +1,63 @@
-import * as WebSocket from 'websocket';
+import * as WebSocket from 'ws';
 import * as http from 'http';
 import * as MQTT from 'mqtt';
-
-const server = http.createServer();
-server.listen(9009, function() {
-    console.log((new Date()) + ' Server is listening on port 9009');
-});
-
-const wss = new WebSocket.server({ httpServer: server });
+import {URL} from 'url';
 
 const client = MQTT.connect(process.env.MQTT_SERVER);
 
-let connection: WebSocket.connection;
+const wss = new WebSocket.Server({ port: 9009 });
 
-wss.on('request', function(request) {
-    connection = request.accept(undefined, request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8' && message.utf8Data !== undefined) {
-            let parsedMessage = JSON.parse(message.utf8Data);
-            let retain = parsedMessage.deviceType === 'sensorDiscrete' ? false : true;
-            let level = typeof parsedMessage.metrics.level !== 'undefined' ? parsedMessage.metrics.level : parsedMessage.metrics.lastLevel;
-            if(typeof level !== 'undefined' && level !== '') {
-                client.publish(`zway/${parsedMessage.id}/status`, level.toString(), { qos: 1, retain: retain});
-            }
+const wsMap = new Map<string,WebSocket>();
+
+wss.on('connection', function connection(ws, req){
+    let device_id: string;
+    console.log((new Date()) + ' - Connection accepted.');
+
+    ws.on('message', (message) => {
+        let stringMessage = message.toString();
+        // if the message is not JSON, store it as the device_id
+        if(stringMessage[0] !== '{'){
+            console.log(`Device id: ${stringMessage}`);
+            device_id = stringMessage;
+            wsMap.set(stringMessage,ws);
+            return;
+        }
+
+        // ignore messages until a device_id has been received
+        if(device_id == null) { return; }
+
+        // parse json message and send an MQTT message
+        let parsedMessage = JSON.parse(stringMessage);
+        let retain = parsedMessage.deviceType === 'sensorDiscrete' ? false : true;
+        let level = parsedMessage.metrics.level ? parsedMessage.metrics.level : parsedMessage.metrics.lastLevel;
+        if(typeof level !== 'undefined' && level !== '') {
+            client.publish(`zwave/${device_id}/${parsedMessage.id}/status`, level.toString(), { qos: 1, retain: retain});
         }
     });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+
+    ws.on('close', () => wsMap.delete(device_id));
 });
 
-client.on('connect', function() {
-    console.log('MQTT Connected');
-    client.subscribe('zway/+/set');
-});
+client.on('message', (topic, message) => {
+    console.log(`${topic} - ${message}`);
+    let topicParts = topic.split('/');
+    let device_id = topicParts[1];
+    let jsonMessage = {
+        id: topicParts[2],
+        command: topicParts[3],
+        value: message.toString()
+    };
 
-client.on('error', function(error) {
-    console.log(`Error: ${error.message}`);
-});
-
-client.on('offline', function() {
-    console.log('MQTT Offline');
-});
-
-client.on('close', function() {
-    console.log('MQTT Closed');
-});
-
-client.on('message', function(topic, message) {
-    if(connection){
-        let topicParts = topic.split('/');
-        let jsonMessage = {
-            id: topicParts[1],
-            command: topicParts[2],
-            value: message.toString()
-        };
-        connection.sendUTF(JSON.stringify(jsonMessage));
+    let ws = wsMap.get(device_id);
+    if(ws === undefined){
+        console.log(`No websocket connection for ${device_id}`);
+        return;
     }
+
+    ws.send(JSON.stringify(jsonMessage));
+});
+
+client.on('connect', () => {
+    console.log('MQTT Connected');
+    client.subscribe('zwave/+/+/set');
 });
